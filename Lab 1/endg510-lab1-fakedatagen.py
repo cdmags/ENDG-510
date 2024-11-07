04654cea909ab90c9d15b958e9dbb42056635abf
cdmags <christiandaniel.mags@ucalgary.ca>
2024-11-06 17:06:43 -0700

    added fake data code

--- Lab 1/endg510-lab1-fakedatagen.py
+++ Lab 1/endg510-lab1-fakedatagen.py
@@ -1,21 +1,14 @@
 import pandas as pd # in case of error, install pnadas using: pip install pandas
 # Read the CSV file into a DataFrame
-import numpy as np
 
 df = pd.read_csv('data.csv')
 
-def new_data():
-    new_data = {
-    'Temp': np.random.uniform(low = 200, high = 300),
-    'Humd': np.random.uniform(low = 300, high = 1000),
+new_data = {
+    'Temp': 366,
+    'Humd': 901,
     'Label': 0
     }
-    return new_data
-
-i = 0
-while i < 50:
-    df = df._append(new_data(), ignore_index=True)
-    i = i+1
+df = df._append(new_data, ignore_index=True)
 
 # Step 3: Save the DataFrame to a new CSV file
 df.to_csv("data.csv", index=False)
